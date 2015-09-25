a3064927ad4d343d0c2a3058cb16750a0371211c
Johan Walles <johan.walles@gmail.com>
2015-09-25 02:55:15 +0200

    Remove dead code

--- app/src/test/java/com/gmail/walles/johan/exactype/GestureDetectorTest.java
+++ app/src/test/java/com/gmail/walles/johan/exactype/GestureDetectorTest.java
@@ -67,9 +67,6 @@ public class GestureDetectorTest {
             this.runnable = runnable;
             this.timeout = timeout;
         }
-        public void run() {
-            runnable.run();
-        }
     }
 
     @Before
